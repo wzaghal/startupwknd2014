e22d2b770cd63037bc643cffc997ab9b4260fb76
wzaghal <wisamzaghal@gmail.com>
2014-01-25 21:46:41 -0500

    added form to /subscriptions/new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Test12::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :subscribers
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
