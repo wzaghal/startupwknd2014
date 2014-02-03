9293e7dd00c74a2fe010a375367f5c71915aeb63
wzaghal <wisamzaghal@gmail.com>
2014-02-02 19:13:31 -0500

    db migrations and reset

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ Test12::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
